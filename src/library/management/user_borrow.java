4c5ad6afd31c6887c5ae207dda03a47510053374
mischmasch <edsellec@gmail.com>
2017-10-24 23:39:23 +0800

    Add files via upload

--- src/library/management/user_borrow.java
+++ src/library/management/user_borrow.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * the user borrows a book and log it to his/her history
  */
 package library.management;
 
